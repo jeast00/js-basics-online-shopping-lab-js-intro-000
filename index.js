5ee10007c823a0b905f20d13f5a05f536bca6e0a
Jonathan East <jonathan_east@ymail.com>
2020-03-05 02:19:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,7 +54,6 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  cart = [];
   if (cardNumber) {
     var string = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
     cart.splice(0, cart.length);
