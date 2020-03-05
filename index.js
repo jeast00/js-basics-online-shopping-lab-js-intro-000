3ac3f723453a834541d916e89d1dc793a6e5d894
Jonathan East <jonathan_east@ymail.com>
2020-03-05 01:50:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
-    if (cart.length === 0){
-      return `Your shopping cart is empty.`;
-    } else if(cart.length === 1) {
+      if(cart.length === 1) {
       return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`;
     } else if (cart.length === 2) {
       return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}, and ${cart[i+1].itemName} at $${cart[i+1].itemPrice}.`;
