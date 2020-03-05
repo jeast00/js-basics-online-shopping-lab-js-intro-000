b4d13040ae78d5c7864de8dcb4ed252607a15fdb
Jonathan East <jonathan_east@ymail.com>
2020-03-05 02:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ function placeOrder(cardNumber) {
   if (cardNumber) {
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
-  cart.splice(0);
+  cart = [];
   return `Sorry, we don't have a credit card on file for you.`;
 }
