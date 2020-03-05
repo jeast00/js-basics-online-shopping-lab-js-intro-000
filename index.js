d146a41150714b867d367ff3f4582fb1a2b1f99f
Jonathan East <jonathan_east@ymail.com>
2020-03-05 02:08:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,5 +54,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber) {
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    cart = [];
+  }
   return `Sorry, we don't have a credit card on file for you.`;
 }
